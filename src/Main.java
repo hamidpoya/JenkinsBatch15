9b376eebef8dfac564bcaeda794bde5876df0ab7
hamidpoya <officepoya@gmail.com>
2023-06-04 13:18:59 -0700

    Second Commit with Rafik Teting Jenkins

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 4; i++) {
             System.out.println("Hello world!!!");
+            System.out.println("This is just a test to check Jenkins");
             Thread.sleep(2000);
                  }
 
